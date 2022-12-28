9ed2286be0a0498312c410fe0c8596c766d9fc60
Percy Ma <kecrily@gmail.com>
2022-12-28 23:12:30 +0800

    chore: clean up extraneous files before syncing

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,7 +1,7 @@
 import degit from 'degit'
 import { cp, rm } from 'node:fs/promises'
 
-degit('eslint/eslint/docs', { force: true}).clone('docs')
+degit('eslint/eslint/docs', { force: true }).clone('docs')
   .finally(async() => {
     await cp('docs', '..', { recursive: true, force: false })
     rm('docs', { recursive: true })
